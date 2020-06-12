4e2d551c90178161358b93cd4a00699a2508d2c7
Philip Daniels <Philip.Daniels1971@gmail.com>
2020-06-12 15:41:55 +0100

    Add initial packages.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use chrono::{DateTime, Utc};
+use env_logger::Builder;
+use log::info;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    configure_logging();
+    info!("Starting rtest");
+
+    info!("Stopping rtest");
+}
+
+// Just configures logging in such a way that we can see everything.
+fn configure_logging() {
+    let mut builder = Builder::from_default_env();
+    builder.format(|buf, record| {
+        let utc: DateTime<Utc> = Utc::now();
+
+        write!(
+            buf,
+            "{:?} {} [{}] ",
+            //utc.format("%Y-%m-%dT%H:%M:%S.%fZ"),
+            utc, // same, probably faster?
+            record.level(),
+            record.target()
+        )?;
+
+        match (record.file(), record.line()) {
+            (Some(file), Some(line)) => write!(buf, "[{}/{}] ", file, line),
+            (Some(file), None) => write!(buf, "[{}] ", file),
+            (None, Some(_line)) => write!(buf, " "),
+            (None, None) => write!(buf, " "),
+        }?;
+
+        writeln!(buf, "{}", record.args())
+    });
+
+    builder.init();
 }
