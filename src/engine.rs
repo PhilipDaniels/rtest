54d9a5e9b716542ae586186a90c2c50f140ab28f
Philip Daniels <Philip.Daniels1971@gmail.com>
2020-06-21 22:29:22 +0100

    Refactoring Engine.

--- src/engine.rs
+++ src/engine.rs
@@ -4,115 +4,81 @@ use std::collections::VecDeque;
 use std::sync::{Arc, Condvar, Mutex};
 use std::thread::{self, JoinHandle};
 
-pub struct JobQueue {
-    jobs: Mutex<VecDeque<Job>>,
-    cvar: Condvar,
-}
-
-impl JobQueue {
-    pub fn new() -> Self {
-        Self {
-            jobs: Mutex::new(VecDeque::<Job>::new()),
-            cvar: Condvar::new(),
-        }
-    }
-
-    pub fn clear(&mut self) {
-        let mut lock = self.jobs.lock().unwrap();
-        lock.clear();
-    }
-
-    pub fn is_empty(&self) -> bool {
-        self.len() == 0
-    }
-
-    pub fn len(&self) -> usize {
-        let lock = self.jobs.lock().unwrap();
-        lock.len()
-    }
-
-    pub fn push_back(&self, job: Job) {
-        assert!(job.is_pending());
-        let mut lock = self.jobs.lock().unwrap();
-        lock.push_back(job);
-        self.cvar.notify_all();
-    }
-
-    // pub fn pop_front(&self, job: Job) -> Option<Job> {
-    //     let mut lock = self.jobs.lock().unwrap();
-    //     lock.pop_front()
-    // }
-
-    fn get_next_pending_job(&self) -> &mut Job {
-        let mut jobs = self.jobs.lock().unwrap();
-        loop {
-            let job = jobs.iter_mut().find(|j| j.is_pending());
-            match job {
-                Some(job) => return job,
-                None => {
-                    info!("All jobs processed, sleeping");
-                    jobs = self.cvar.wait(jobs).unwrap();
-                }
-            }
-
-            // match jobs.pop_front() {
-            //     Some(job) => return job,
-            //     None => {
-            //         info!("All jobs processed, sleeping");
-            //         jobs = self.cvar.wait(jobs).unwrap();
-            //     }
-            // }
-        }
-    }
-}
+type JobQ = Arc<Mutex<VecDeque<Job>>>;
 
 pub struct JobEngine {
-    pending_jobs: Arc<JobQueue>,
-    completed_jobs: Arc<Mutex<Vec<Job>>>,
+    pending_jobs: JobQ,
+    completed_jobs: JobQ,
+    pending_jobs_signal_variable: Condvar,
     job_processor: JoinHandle<()>,
 }
 
 /// Based on https://www.poor.dev/posts/what-job-queue/.
 impl JobEngine {
     pub fn new() -> Self {
-        let pending_jobs = Arc::new(JobQueue::new());
-        let completed_jobs = Arc::new(Mutex::new(Vec::new()));
+        let pending_jobs = Arc::new(Mutex::new(VecDeque::<Job>::new()));
+        let completed_jobs = Arc::new(Mutex::new(VecDeque::<Job>::new()));
 
         let job_processor = thread::spawn({
             let pending_jobs = pending_jobs.clone();
             let completed_jobs = completed_jobs.clone();
 
             move || loop {
-                let job = pending_jobs.get_next_pending_job();
-                job.execute();
-                //let mut cj = completed_jobs.lock().unwrap();
-                //cj.push(job);
+                let mut job_lock = pending_jobs.lock().unwrap();
+                let next_job = job_lock.iter_mut().find(|job: &&mut Job|  job.is_pending());
+                match next_job {
+                    Some(job) => {
+                        job.execute();
+                        // Can't do this yet, need to remove the job from the pending_jobs queue.
+                        // let mut cj = completed_jobs.lock().unwrap();
+                        // cj.push_back(job);
+                    }
+                    None => {
+                        info!("All jobs processed, sleeping");
+                        //let jobs = pending_jobs_signal_variable.wait(jobs).unwrap();
+                    }
+                }
             }
         });
 
         Self {
             pending_jobs,
             completed_jobs,
+            pending_jobs_signal_variable: Condvar::new(),
             job_processor,
         }
     }
 
+    fn process_jobs(&self) {
+
+    }
+
     pub fn add_job(&mut self, job: Job) {
-        self.pending_jobs.push_back(job);
+        assert!(job.is_pending());
+        let mut job_lock = self.pending_jobs.lock().unwrap();
+        job_lock.push_back(job);
+        // Tell everybody listening (really it's just us) that there is now
+        // a job in the pending queue.
+        self.pending_jobs_signal_variable.notify_all();
+    }
+
+    pub fn num_pending(&self) -> usize {
+        let job_lock = self.pending_jobs.lock().unwrap();
+        job_lock.len()
     }
 
     pub fn is_empty(&self) -> bool {
-        self.pending_jobs.is_empty()
+        let job_lock = self.pending_jobs.lock().unwrap();
+        job_lock.is_empty()
     }
 
-    pub fn len(&self) -> usize {
-        self.pending_jobs.len()
-    }
-
-    pub fn completed_len(&self) -> usize {
-        self.completed_jobs.lock().unwrap().len()
+    pub fn num_completed_len(&self) -> usize {
+        let job_lock = self.completed_jobs.lock().unwrap();
+        job_lock.len()
     }
 }
+
+
 
 /*
 We need the following
