4b833bb788b3543adbf8cb9183170fdcd33ddac0
Philip Daniels <Philip.Daniels1971@gmail.com>
2020-06-29 16:42:42 +0100

    Remove old engine code. Tidy comments a bit.

--- src/engine.rs
+++ src/engine.rs
@@ -5,7 +5,7 @@ use std::sync::{
     mpsc::{channel, Receiver, Sender},
     Arc, Condvar, Mutex,
 };
-use std::thread::{self, JoinHandle};
+use std::thread;
 
 #[derive(Debug, Clone)]
 pub struct JobEngine {
@@ -50,11 +50,6 @@ struct JobEngineInner2 {
     // add new jobs to the queue, because that is controlled by a different thread.
     job_starter_clutch: ThreadClutch,
 
-    // A sender channel for communicating with the JOB_EXECUTOR thread.
-    // It needs to be wrapped in a mutex so that we can clone
-    // engines and send them across threads.
-    //job_exec_sender: Mutex<Sender<Job>>,
-
     // A channel to handle the addition of jobs to the queue.
     // The other end of this channel is monitored by the JOB_ADDER thread.
     job_adder_sender: Mutex<Sender<Job>>,
@@ -90,7 +85,6 @@ impl JobEngineInner2 {
             pending_jobs,
             completed_jobs,
             job_starter_clutch,
-            //job_exec_sender: Mutex::new(job_exec_sender),
             job_adder_sender: Mutex::new(job_adder_sender),
             job_adder_signal,
         }
@@ -99,10 +93,12 @@ impl JobEngineInner2 {
     pub fn start(&self) {}
 
     pub fn pause(&self) {
+        info!("JobEngine paused");
         self.job_starter_clutch.pause_threads();
     }
 
     pub fn restart(&self) {
+        info!("JobEngine restarting");
         self.job_starter_clutch.release_threads();
     }
 
@@ -123,6 +119,7 @@ impl JobEngineInner2 {
         builder
             .spawn(move || {
                 for mut job in job_exec_internal_receiver {
+                    // TODO: Tidy up the JobState management.
                     job.execute();
                     job_exec_internal_sender
                         .send(job)
@@ -144,7 +141,7 @@ impl JobEngineInner2 {
                 for job in receiver {
                     let mut lock = pending_jobs.lock().unwrap();
                     info!(
-                        "Added {}, there are now {} jobs in the queue",
+                        "Added {}, there are now {} jobs in the pending queue",
                         job,
                         lock.len() + 1
                     );
@@ -240,83 +237,6 @@ impl JobEngineInner2 {
         }
 
         None
-    }
-}
-
-#[derive(Debug, Clone, Default)]
-struct JobEngineInner {
-    pending_jobs: Arc<Mutex<VecDeque<Job>>>,
-    completed_jobs: Arc<Mutex<VecDeque<Job>>>,
-    pending_job_condvar: Arc<Condvar>,
-    pause_queue_clutch: ThreadClutch,
-    //queue_manager_join_handle: Option<JoinHandle<()>>,
-}
-
-const JOB_EXECUTOR_THREAD_NAME: &str = "JOB_EXECUTOR";
-
-/// Based on https://www.poor.dev/posts/what-job-queue/.
-impl JobEngineInner {
-    pub fn new() -> Self {
-        Self {
-            pending_jobs: Arc::new(Mutex::new(VecDeque::new())),
-            completed_jobs: Arc::new(Mutex::new(VecDeque::new())),
-            pending_job_condvar: Arc::new(Condvar::new()),
-            //queue_manager_join_handle: None,
-            pause_queue_clutch: Default::default(),
-        }
-    }
-
-    pub fn start(&mut self) {
-        // If there is a queue manager, we're already started.
-        // if self.queue_manager_join_handle.is_some() {
-        //     return;
-        // }
-
-        info!("Starting job engine");
-
-        self.create_queue_mgr_thread();
-    }
-
-    fn create_queue_mgr_thread(&mut self) {
-        const QUEUE_MGR_THREAD_NAME: &str = "QUEUE_MGR";
-
-        let jobs = self.pending_jobs.clone();
-        let pending_job_condvar = self.pending_job_condvar.clone();
-        let clutch = self.pause_queue_clutch.clone();
-        let builder = thread::Builder::new().name(QUEUE_MGR_THREAD_NAME.into());
-
-        //self.queue_manager_join_handle =
-        builder
-            .spawn(move || {
-                // The loop makes this thread run forever.
-                loop {
-                    clutch.wait_for_release();
-
-                    // If we get to here then the worker thread is running. There may or may
-                    // not be any pending jobs to process. Execute at most one job (so that
-                    // there is a chance to pause between jobs).
-                    let mut jobs_guard = jobs.lock().unwrap();
-                    match jobs_guard
-                        .iter_mut()
-                        .find(|job: &&mut Job| job.is_pending())
-                    {
-                        Some(next_job) => next_job.execute(),
-                        None => {
-                            info!("{}: All jobs processed, sleeping", QUEUE_MGR_THREAD_NAME);
-                            // Here we just wait for work to become available. We don't care about the
-                            // return value of `wait`, because we will re-get `jobs_guard` above,
-                            // when we come round the loop again.
-                            let _ = pending_job_condvar.wait(jobs_guard).unwrap();
-                        }
-                    }
-                }
-            })
-            .expect(&format!(
-                "{} Failed to create the thread",
-                QUEUE_MGR_THREAD_NAME
-            ));
-
-        info!("{}: Successfully spawned the thread", QUEUE_MGR_THREAD_NAME);
     }
 }
 
