d719ef3ed6e27364d8c46bd770a26aebea4aec0b
Philip Daniels <Philip.Daniels1971@gmail.com>
2020-07-09 16:44:53 +0100

    Remove unnecessary threads.

--- src/engine.rs
+++ src/engine.rs
@@ -1,13 +1,13 @@
 use crate::{
-    jobs::{BuildJob, BuildMode, Job, JobKind, ShadowCopyJob, PendingJob, CompletedJob, ExecutingJob, CompletionStatus},
+    jobs::{BuildJob, BuildMode, CompletedJob, CompletionStatus, Job, JobKind, PendingJob},
     shadow_copy_destination::ShadowCopyDestination,
     thread_clutch::ThreadClutch,
 };
 use log::info;
 use std::collections::VecDeque;
 use std::sync::{
-    mpsc::{channel, Receiver, Sender},
-    Arc, Condvar, Mutex, MutexGuard, atomic::{Ordering, AtomicBool},
+    atomic::{AtomicBool, Ordering},
+    Arc, Condvar, Mutex, MutexGuard,
 };
 use std::thread;
 
@@ -30,8 +30,6 @@ ELSE
 Flag for 'is a build required'?
 */
 
-type JobList = Arc<Mutex<VecDeque<PendingJob>>>;
-
 #[derive(Debug, Clone)]
 pub struct JobEngine {
     dest_dir: ShadowCopyDestination,
@@ -39,7 +37,7 @@ pub struct JobEngine {
     /// The list of pending (yet to be executed) jobs.
     pending_jobs: Arc<Mutex<VecDeque<PendingJob>>>,
 
-    executing_job: Option<ExecutingJob>,
+    executing_job: Arc<Mutex<Option<PendingJob>>>,
 
     /// The list of completed jobs.
     completed_jobs: Arc<Mutex<VecDeque<CompletedJob>>>,
@@ -71,53 +69,15 @@ impl JobEngine {
             build_required: Default::default(),
         };
 
-        // These channels are used to connect up the various threads.
-        let (job_exec_sender, job_exec_internal_receiver) = channel::<PendingJob>();
-        let (job_exec_internal_sender, job_exec_receiver) = channel::<CompletedJob>();
-
-        // Create the JOB_EXECUTOR thread. This thread just calls
-        // `Job.execute()`, one job at a time. It receives jobs on a channel, and sends
-        // the results back on another channel (where they are picked up by
-        // the JOB_COMPLETED thread).
+        // Start the JOB_EXECUTOR thread. This thread picks jobs off the front
+        // of the queue and executes them one at a time.
         let builder = thread::Builder::new().name("JOB_EXECUTOR".into());
         builder
             .spawn({
-                let this = this.clone();
-                move || {
-                    this.run_job_executor_thread(
-                        job_exec_internal_receiver,
-                        job_exec_internal_sender,
-                    )
-                }
+                let mut this = this.clone();
+                move || this.execute_jobs()
             })
             .expect("Cannot create JOB_EXECUTOR thread");
-
-        // Create the JOB_STARTER thread. This thread is responsible for checking the
-        // `pending_jobs` queue to see if there are any jobs that need executing, and if
-        // there are it clones them and sends them to the JOB_EXECUTOR thread.
-        // If there are no pending jobs then it goes to sleep, until it is woken up by
-        // `add_job` notifying the signal.
-        // Q: Why not use a channel with a for loop like the other threads, since that would
-        // allow us to dispense with the Condvar? A: We want the list of pending jobs to
-        // be observable / iterable, so we must maintain our own queue, we can't use the
-        // channels queue because iterating it consumes the items.
-        let builder = thread::Builder::new().name("JOB_STARTER".into());
-        builder
-            .spawn({
-                let this = this.clone();
-                move || this.run_job_starter_thread(job_exec_sender)
-            })
-            .expect("Cannot create JOB_STARTER thread");
-
-        // Create the JOB_COMPLETED thread. It is the job of this thread to listen
-        // for completed job messages which are sent by the JOB_EXECUTOR thread.
-        let builder = thread::Builder::new().name("JOB_COMPLETED".into());
-        builder
-            .spawn({
-                let this = this.clone();
-                move || this.run_job_completed_thread(job_exec_receiver)
-            })
-            .expect("Cannot create JOB_COMPLETED thread");
 
         this
     }
@@ -137,20 +97,6 @@ impl JobEngine {
         self.job_starter_clutch.release_threads();
     }
 
-    fn add_job_inner(&self, job: PendingJob, mut pending_jobs_guard: MutexGuard<VecDeque<PendingJob>>) {
-        info!(
-            "Added {}, there are now {} jobs in the pending queue",
-            job,
-            pending_jobs_guard.len() + 1
-        );
-
-        pending_jobs_guard.push_back(job);
-
-        // Tell everybody listening (really it's just us with one thread) that there
-        // is now a job in the pending queue.
-        self.job_added_signal.notify_all();
-    }
-
     /// Add a job to the end of the queue.
     pub fn add_job(&self, job: PendingJob) {
         // This lock won't block the caller much, because all other locks
@@ -159,29 +105,42 @@ impl JobEngine {
         self.add_job_inner(job, pending_jobs_guard);
     }
 
-    fn run_job_executor_thread(
-        &self,
-        job_exec_internal_receiver: Receiver<PendingJob>,
-        job_exec_internal_sender: Sender<CompletedJob>,
-    ) {
-        for job in job_exec_internal_receiver {
-            let job = job.execute();
-            job_exec_internal_sender
-                .send(job)
-                .expect("Cannot return job from JOB_EXECUTOR");
-        }
-    }
-
-    fn run_job_starter_thread(&self, job_exec_sender: Sender<PendingJob>) {
+    fn execute_jobs(&mut self) {
         let dummy_mutex = Mutex::new(());
 
         loop {
+            // If we are paused, wait until we are released.
             self.job_starter_clutch.wait_for_release();
 
+            // Do we have a job to execute?
             if let Some(job) = self.get_next_job() {
-                job_exec_sender
-                    .send(job)
-                    .expect("Could not send job to JOB_EXECUTOR thread");
+                //                self.executing_job = Some(job.clone());
+                // This is potentially time consuming, everything else in this
+                // method should be fast (hence the locks will be released quickly).
+                let completed_job = job.execute();
+                //                self.executing_job = None;
+
+                self.set_flags(&completed_job);
+                let pending_jobs_lock = self.pending_jobs.lock().unwrap();
+                let mut completed_jobs_lock = self.completed_jobs.lock().unwrap();
+
+                let msg = format!(
+                    "{} completed, there are now {} pending and {} completed jobs",
+                    completed_job,
+                    pending_jobs_lock.len(),
+                    completed_jobs_lock.len() + 1
+                );
+
+                completed_jobs_lock.push_back(completed_job);
+                drop(completed_jobs_lock);
+
+                info!("{}", msg);
+
+                if pending_jobs_lock.is_empty() {
+                    if self.build_required.load(Ordering::SeqCst) {
+                        self.add_build_job(pending_jobs_lock);
+                    }
+                }
             } else {
                 // The idea here is that this will BLOCK and you are not allowed to touch the
                 // data guarded by the MUTEX until the signal happens.
@@ -196,33 +155,6 @@ impl JobEngine {
         pending_jobs_guard.pop_front()
     }
 
-    fn run_job_completed_thread(&self, job_exec_receiver: Receiver<CompletedJob>) {
-        for job in job_exec_receiver {
-            self.set_flags(&job);
-
-            let pending_jobs_lock = self.pending_jobs.lock().unwrap();
-            let mut completed_jobs_lock = self.completed_jobs.lock().unwrap();
-
-            let msg = format!(
-                "{} completed, there are now {} pending and {} completed jobs",
-                job,
-                pending_jobs_lock.len(),
-                completed_jobs_lock.len() + 1
-            );
-
-            completed_jobs_lock.push_back(job);
-            drop(completed_jobs_lock);
-
-            info!("{}", msg);
-
-            if pending_jobs_lock.is_empty() {
-                if self.build_required.load(Ordering::SeqCst) {
-                    self.add_build_job(pending_jobs_lock);
-                }
-            }
-        }
-    }
-
     /// Convenince method to add a new build job.
     /// TODO: In the future this might be more sophisticated, for example checking to see
     /// if there is an existing build job already in the pipeline and moving it to the end (if it's
@@ -232,6 +164,24 @@ impl JobEngine {
         self.add_job_inner(job, pending_jobs_guard);
     }
 
+    fn add_job_inner(
+        &self,
+        job: PendingJob,
+        mut pending_jobs_guard: MutexGuard<VecDeque<PendingJob>>,
+    ) {
+        info!(
+            "{} added, there are now {} jobs in the pending queue",
+            job,
+            pending_jobs_guard.len() + 1
+        );
+
+        pending_jobs_guard.push_back(job);
+
+        // Tell everybody listening (really it's just us with one thread) that there
+        // is now a job in the pending queue.
+        self.job_added_signal.notify_all();
+    }
+
     fn set_build_required_flag(&self, value: bool) {
         self.build_required.store(value, Ordering::SeqCst);
     }
@@ -239,15 +189,25 @@ impl JobEngine {
     /// Sets the various state flags based on the job and its completion status.
     fn set_flags(&self, job: &CompletedJob) {
         match (job.kind(), job.completion_status()) {
-            (JobKind::ShadowCopy(_), crate::jobs::CompletionStatus::Ok) => self.set_build_required_flag(true),
-            (JobKind::ShadowCopy(_), crate::jobs::CompletionStatus::Error(_)) => self.set_build_required_flag(false),
+            (JobKind::ShadowCopy(_), crate::jobs::CompletionStatus::Ok) => {
+                self.set_build_required_flag(true)
+            }
+            (JobKind::ShadowCopy(_), crate::jobs::CompletionStatus::Error(_)) => {
+                self.set_build_required_flag(false)
+            }
 
-            (JobKind::FileSync(_), crate::jobs::CompletionStatus::Ok) => self.set_build_required_flag(true),
+            (JobKind::FileSync(_), crate::jobs::CompletionStatus::Ok) => {
+                self.set_build_required_flag(true)
+            }
             (JobKind::FileSync(_), crate::jobs::CompletionStatus::Error(_)) => {}
 
-            (JobKind::Build(_), crate::jobs::CompletionStatus::Ok) => self.set_build_required_flag(false),
+            (JobKind::Build(_), crate::jobs::CompletionStatus::Ok) => {
+                self.set_build_required_flag(false)
+            }
             // TODO: This is a problem. Will rebuild infinitely.
-            (JobKind::Build(_), crate::jobs::CompletionStatus::Error(_)) => self.set_build_required_flag(true),
+            (JobKind::Build(_), crate::jobs::CompletionStatus::Error(_)) => {
+                self.set_build_required_flag(true)
+            }
 
             (_, CompletionStatus::Unknown) => {}
         }
